cb0045a5f1f00b405d3eca0d954e800c0a4d3c03
neolectron <xeolectron@gmail.com>
2016-12-19 03:12:12 +0100

    settings

--- app/config/core.js
+++ app/config/core.js
@@ -1,4 +1,12 @@
+exports.node_env    = process.env.node_env || 'developpement';
 exports.title       = 'ExpressBones';
 exports.port        = process.env.port     || 3000
 exports.dburl       = process.env.dburl    || 'mongodb://codinglab.io:27017/expressbones';
-exports.node_env    = process.env.node_env || 'developpement';
+exports.session		= 
+{
+	secret: 'such secret, much wow, marvellous',
+	saveUninitialized: true,
+	resave: false,
+	//store: MemoryStore,      //please consider changing the store, MemoryStore is not good for production
+	cookie : { httpOnly: true, maxAge: 2419200000 }
+};
